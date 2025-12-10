6dd6416958edd45643bbef01c67ad10ce4bc16b0
Christoph Wiltschko <is241319@fhstp.ac.at>
2025-12-10 15:54:07 +0100

    add nftables.conf

--- branch/fw-branch/cmd.sh
+++ branch/fw-branch/cmd.sh
@@ -1,5 +1,9 @@
+#enable ip forwarding
 echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
+#applies new sysctl.conf
 sysctl -p
 
+#starts nftables service
 systemctl start nftables.service
+#ensures nftables is started after reboot
 systemctl enable nftables.service
